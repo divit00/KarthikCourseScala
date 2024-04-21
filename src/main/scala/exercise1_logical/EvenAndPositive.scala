bf2602815cb00dfd1213bc792e55f6886b946ed3
divit00 <67533750+divit00@users.noreply.github.com>
2024-04-21 22:12:49 +0530

    second commit

--- src/main/scala/exercise1_logical/EvenAndPositive.scala
+++ src/main/scala/exercise1_logical/EvenAndPositive.scala
@@ -1,5 +1,22 @@
 package main.scala.exercise1_logical
 
+import scala.io.StdIn._
+//Write a Scala function to check if a given number is both even and positive.
+//Sample Data: 14
+
 object EvenAndPositive {
+
+  def main(args: Array[String]): Unit = {
+
+    val input_number = readInt()
+    println("Input Number : " + input_number)
+
+    if (input_number > 0 & input_number % 2 == 0) {
+      println("Input Number is positive and even")
+    }
+    else {
+      println("Input Number is not positive and even")
+    }
+  }
 
 }
